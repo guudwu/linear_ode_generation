da39435fc306f35f955562c8d8aa6689389add27
GuuD WU <guuuuud@gmail.com>
2013-11-25 17:43:59 -0500

    Add output argument in "check.linear_ode.R"
    Add output argument "max_correlation" as previous "correlation".
    Now "correlation" outputs the whole pair-wise correlation matrix.
    
    Add comments
    
    Add sanity check.

--- check.linear_ode.R
+++ check.linear_ode.R
@@ -1,3 +1,9 @@
+# Check property of generated ODE system.
+
+# Currently only two properties are checked:
+# Pair-wise correlation of curves.
+# Variance Inflation Factor.
+
 check.linear_ode <- function
 (
   linear_ode
@@ -5,15 +11,39 @@ check.linear_ode <- function
   , vif = TRUE
 )
 
+# INPUT:
+# linear_ode: Return value of "linear_ode_generation.R".
+# correlation: Whether to check pair-wise correlation.
+# vif: Whether to check Variance Inflation Factor.
+
+# OUTPUT:
+# correlation: Pair-wise correlation matrix.
+# max_correlation: Maximal correlation, other than diagonal elements.
+# vif: Variance Inflation Factor.
+
 {
+
+# Sanity check
+
+correlation = as.logical(correlation)
+if ( length(correlation)!=1 )
+{
+  stop('"correlation" must be a logical scalar.')
+}
+
+vif = as.logical(correlation)
+if ( length(vif)!=1 )
+{
+  stop('"vif" must be a logical scalar.')
+}
 
 # Correlation
 
 if ( correlation )
 {
   correlation <- cor ( linear_ode$observation[,-1] )
-  correlation <- correlation - diag(ncol(linear_ode$observation)-1)
-  correlation <- max(abs(correlation))
+  max_correlation <- correlation - diag(ncol(linear_ode$observation)-1)
+  max_correlation <- max(abs(max_correlation))
 }
 
 # Variation Inflation Factor
@@ -35,6 +65,7 @@ if ( vif )
 
 return ( list (
   correlation = correlation
+  , max_correlation = max_correlation
   , vif = vif
 ) )
 
